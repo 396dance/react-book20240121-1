fe676d2499e8b4b9d7c9282bfa972c716430d80f
Mikuno Yasuhiro <396dance@gmail.com>
2024-01-30 21:16:26 +0900

    Childファイル作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,25 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { ColordMessage } from "./components/ColordMessage";
+import Child1 from "./components/Child1.tsx";
+import Child4 from "./components/Child4.tsx";
 
 function App() {
-  // 入力フォームの中身をState管理する
-  const [text, setText] = useState<string>("");
-  const [memos, setMemos] = useState<string[]>([]);
-  const [num, setNum] = useState(0);
+  console.log("Appレンダリング");
 
-  const onChangeText = (e: ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value);
-  };
+  const [num, setNum] = useState<number>(0);
 
   const onClickAdd = () => {
     setNum(num + 1);
-    const newMemos = [...memos];
-    newMemos.push(text);
-    setMemos(newMemos);
-    setText("");
-  };
-
-  const contentPinkStyle = {
-    color: "pink",
-    fontSize: "20px",
   };
 
   return (
     <div className="App">
       <h1>簡単メモアプリ</h1>
-      <input type="text" value={text} onChange={onChangeText} />
-      <button onClick={onClickAdd}>追加</button>
-      <h2>メモ一覧</h2>
-      <ul>
-        {memos.map((memo, index) => (
-          <li key={memo}>
-            <p>{memo}</p>
-          </li>
-        ))}
-      </ul>
-      <h2>メモの件数</h2>
+      <button onClick={onClickAdd}>ボタン</button>
       <p>{num}</p>
-      <p style={contentPinkStyle}>元気です！</p>
-      <ColordMessage color="blue" message="お元気ですか？" />
+      <Child1 />
+
+      <Child4 />
     </div>
   );
 }
--- src/components/ColordMessage.jsx
+++ src/components/ColordMessage.jsx
@@ -1,8 +1,8 @@
-export const ColordMessage = (props) => {
-  const contentStyle = {
-    color: props.color,
-    message: props.message,
-    fontSize: "20px",
-  };
-  return <p style={contentStyle}>お元気ですか？</p>;
-};
+// export const ColordMessage = (props) => {
+//   const contentStyle = {
+//     color: props.color,
+//     message: props.message,
+//     fontSize: "20px",
+//   };
+//   return <p style={contentStyle}>お元気ですか？</p>;
+// };
